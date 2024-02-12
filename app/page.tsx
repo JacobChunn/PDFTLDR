fd512f955d660ed40861e4b704e98ae0f0e8479d
StefaniakChristopher <stefaniakchristopher@gmail.com>
2024-02-12 17:51:58 -0500

    test push

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
-
+        {/* hello, thsi si a test */}
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
