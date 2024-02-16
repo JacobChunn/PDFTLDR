c95e00f197c4b7cabfe49b1d1db5d45d5d6d7691
Chris <stefaniakchristopher@gmail.com>
2024-02-16 15:16:47 -0500

    file upload UI init

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,85 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-        {/* hello, thsi si a test */}
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="h-screen [background:var(--bg-secondary)] flex items-center justify-center">
+      <section className="[background:var(--bg-primary)] w-[1200px] h-[800px]">
+        {/* navbar */}
+        <header className="flex justify-between items-center p-4 border-b-2">
+          <div className="ml-8">
+            <h1>PDFTLDR</h1>
+          </div>
+          <div className="[font-family:var(--secondary-font)] font-bold">
+            <ul className="flex gap-[12px]">
+              <li>Eleven</li>
+              <li>Twelve</li>
+              <li>Thirteen</li>
+              <li>Sixteen</li>
+            </ul>
+          </div>
+          <div className="flex gap-[12px]">
+            <button className="border-2 [border-color:var(--button-color)] [color:var(--button-color)] [font-family:var(--primary-font)] p-1 px-3">Button Text</button>
+            <button className="border-2 [background:var(--button-color)] [border-color:var(--button-color)] [color:var(--font-primary)] [font-family:var(--primary-font)] p-1 px-3">Button Text</button>
+          </div>
+        </header>
+        {/* main content */}
+        <main className="flex items-center justify-evenly my-[40px]">
+          {/* the picture */}
+          <div>
+            <img src="/guy.png" alt="guy i found on linkedin" className="w-[500px] h-[500px]"/>
+          </div>
+          {/* upload document section */}
+          <div className="flex flex-col items-center">
+            <h1 className="[font-family:var(--primary-font)] text-4xl font-bold">Upload Document</h1>
+            <h4 className="[font-family:var(--primary-font)] mb-8">Summarize your way, in seconds.</h4>
+            <div className="flex mb-10">
+              <div className="[background:var(--font-primary)] border-b-2 [border-color:var(--bg-secondary)] p-2 text-center px-24 cursor-pointer">PDF, Docx, or text file</div>
+              <button className="border-2 [background:var(--button-color)] [border-color:var(--button-color)] [color:var(--font-primary)] [font-family:var(--primary-font)] p-1 px-3 ml-2">Upload</button>
+            </div>
+            <div className="flex justify-start flex-col items-start w-full mb-10">
+              <p>Save as</p>
+              <input type="text" placeholder="Placeholder" className="[background:var(--font-primary)] border-b-2 [border-color:var(--bg-secondary)] p-2 text-start w-full cursor-pointer"/>
+            </div>
+            <div className="flex justify-between border-2 [border-color:var(--bg-secondary)] mb-16">
+              <div className="flex items-center border-r-2 [border-color:var(--bg-secondary)] p-1 px-5">
+                <div className="w-6 h-6 border-2 [border-color:var(--bg-secondary)] rounded-full p-1 mr-4"></div>
+                <p className="p-1">Title</p>
+              </div>
+              <div className="flex items-center border-r-2 [border-color:var(--bg-secondary)] p-1 px-5">
+                <div className="w-6 h-6 border-2 [border-color:var(--bg-secondary)] rounded-full p-1 mr-4"></div>
+                <p>Title</p>
+              </div>
+              <div className="flex items-center [border-color:var(--bg-secondary)] p-1 px-5">
+                <div className="w-6 h-6 border-2 [border-color:var(--bg-secondary)] rounded-full p-1 mr-4"></div>
+                <p>Title</p>
+              </div>
+            </div>
+            <button className="border-2 [border-color:var(--button-color)] [color:var(--button-color)] [font-family:var(--primary-font)] p-1 px-3 w-full">Summarize</button>
+          </div>
+          {/* footer section */}
+        </main>
+        <footer className="flex flex-col justify-evenly bg-gray-600 h-[150px]">
+            <div className="flex items-center justify-between">
+              <div className="p-4 ml-8">
+                <h3 className="[color:var(--font-primary)] ">PDFTDLR</h3>
+              </div>
+              <div>
+                <ul className="flex gap-[12px] [color:var(--font-primary)]">
+                  <li>Eleven</li>
+                  <li>Twelve</li>
+                  <li>Thirteen</li>
+                  <li>Fourteen</li>
+                </ul>
+              </div>
+              <div className="mr-12">
+                ICONS
+                {/* icons go here */}
+              </div>
+            </div>
+            <div className="my-8 flex items-center justify-center text-xs [color:var(--font-primary)]">
+              <p>PDFTDLR @2024. All rights reserved</p>
+            </div>
+          </footer>
+      </section>
+    </div>
   );
 }
