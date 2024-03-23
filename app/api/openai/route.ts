991931a2d3c99a25b745263d40e408a7de452f75
Zachary Lain <zacharytlain@gmail.com>
2024-03-23 19:24:30 -0400

    Refactor OpenAI API call

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -3,21 +3,27 @@ import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
 dotenv.config();
 
+
 const openai = new OpenAI({
   // you need to set this in your local .env file until this is deployed
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 
 export async function POST(request: Request) {
   const bodyBuffer = await new Response(request.body).arrayBuffer();
   const bodyJSON = new TextDecoder("utf-8").decode(bodyBuffer);
   const body = JSON.parse(bodyJSON);
 
+
   const { option, text } = body;
+
 
   console.log(text);
 
+
   let prompt = "";
+
 
   switch (option) {
     case "Paragraph":
@@ -27,42 +33,48 @@ export async function POST(request: Request) {
       prompt = "summarize this text with bullet points:\n ";
       break;
     case "Sentence":
-      prompt = "summarize this text with one sentence only:\n";
+      prompt = "summarize this text with sentence:\n";
       break;
     default:
       prompt = "summarize this text with a paragraph:\n";
       break;
   }
 
-  const chatGPTapiCall = async (prompt: string, text: string) => {
-    try {
-      const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: prompt + text }],
-        model: "gpt-3.5-turbo",
-      });
 
-      const summary = completion.choices[0].message.content;
+  const response = await chatGPTApiCall(prompt, text);
 
-      // if (option === "Bullet Points") {
-      //   //@ts-ignore
-      //   for (let i = 0; i < summary.length; i++) {
-      //     if (summary[i] === "\n") {
-      //       summary = summary.slice(0, i) + " " + summary.slice(i + 1);
-      //     }
-      //   }
-      // }
-
-      console.log(`Summary: ${summary}`);
-
-      return new Response(JSON.stringify(summary));
-    } catch (e) {
-      console.error(e);
-
-      return new Response(JSON.stringify("ChatGPT API call failed."));
-    }
-  };
-
-  const response = await chatGPTapiCall(prompt, text);
 
   return response;
 }
+
+
+export async function chatGPTApiCall(prompt: string, text: string): Promise<Response> {
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: prompt + text }],
+      model: "gpt-3.5-turbo",
+    });
+
+
+
+
+    const summary = completion.choices[0].message.content;
+
+
+
+
+    console.log(`Summary: ${summary}`);
+
+
+
+
+    return new Response(JSON.stringify(summary));
+  } catch (e) {
+    console.error(e);
+
+
+
+
+    return new Response(JSON.stringify("ChatGPT API call failed."));
+  }
+};
