bd27d1d10c938bc041ccdd773d0ececd4f8ca00a
Jake Chunn <chunnjake@gmail.com>
2024-03-23 21:58:41 -0400

    Updated navbar

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
     },
   },
 
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
