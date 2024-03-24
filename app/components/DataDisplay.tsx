0d3c97b3b138a5b8a61911ab9f204389255eac9e
Chris <stefaniakchristopher@gmail.com>
2024-03-23 23:42:21 -0400

    bullet points fix

--- app/components/DataDisplay.tsx
+++ app/components/DataDisplay.tsx
@@ -1,16 +1,22 @@
+"use client";
 import React from "react";
 
 //@ts-ignore
 const DataDisplay = ({ translatedDocument, summaryType }) => {
   if (summaryType === "Bullet Points") {
+    console.log(translatedDocument);
+    console.log(translatedDocument.split("- "));
     return (
-      <>
-        <ul>
-          {translatedDocument.split("-").map((line: any, index: any) => (
-            <li key={index}>{line}</li>
-          ))}
+      <div>
+        <ul className="list-disc list-inside">
+          {translatedDocument
+            .split("- ")
+            .filter((line: string) => line)
+            .map((line: string, index: any) => (
+              <li key={index}>{line}</li>
+            ))}
         </ul>
-      </>
+      </div>
     );
   } else {
     return <p>{translatedDocument}</p>;
