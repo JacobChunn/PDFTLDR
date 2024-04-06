dcdd2a070fdd4deeb4e085fc63eb44a09d04612d
Zachary Lain <zacharytlain@gmail.com>
2024-04-06 19:35:25 -0400

    Creating a temp directory before using parser

--- app/api/parser/route.ts
+++ app/api/parser/route.ts
@@ -1,6 +1,27 @@
 import officeParser from "officeparser";
+import fs from "fs";
+import path from "path";
+import { Buffer } from "buffer";
+
+// Ensure that the /tmp directory exists
+const tempDir = path.join('/tmp', 'officeParserTemp', 'tempfiles');
+
+function ensureDirSync(dirpath : any) {
+  try {
+    return fs.mkdirSync(dirpath, { recursive: true });
+  } catch (err : any) {
+    if (err.code !== 'EEXIST') throw err; // Ignore the error if the directory already exists
+  }
+}
+
+// Ensuring the directory exists in the serverless function
+ensureDirSync(tempDir);
 
 export async function POST(request: Request) {
+  // Ensuring the directory exists during the runtime of your serverless function
+  const tempDir = path.join('/tmp', 'officeParserTemp', 'tempfiles');
+  ensureDirSync(tempDir);
+
   const formData = await request.formData();
   const file = formData.get("file") as File;
   console.log("File name:", file.name, "size:", file.size);
